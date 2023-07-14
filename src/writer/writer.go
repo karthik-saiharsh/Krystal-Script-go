aa5059c9c04ada8aec2481f090f2bc1f02ed071f
Karthik Saiharsh <137990475+KS-the-visionary@users.noreply.github.com>
2023-07-14 17:10:30 +0530

    Add files via upload

--- src/writer/writer.go
+++ src/writer/writer.go
@@ -18,6 +18,8 @@ func write_to_script(flname string, content string) {
 
 func Display(line string, index int, flname string, osname string) {
 	display_pattern := regexp.MustCompile(`^Display\s*\(\s*"(.*?)"\s*\)$`)
+	display_pattern_literal := regexp.MustCompile(`^Display\s*\(\s*literal\s*"(.*?)"\s*\)$`)
+	display_pattern_no_new_line := regexp.MustCompile(`^Display\s*\(\s*no\s+new\s+line\s*"(.*?)"\s*\)$`)
 
 	if display_pattern.MatchString(line) {
 		match := display_pattern.FindStringSubmatch(line)
@@ -25,11 +27,31 @@ func Display(line string, index int, flname string, osname string) {
 			content := "echo \"" + match[1] + "\"\n"
 			write_to_script(flname, content)
 		} else {
-			content := "echo " + match[1] + "\n"
-			write_to_script(flname, content)
+			// Win Code here
 		}
+
+	} else if display_pattern_literal.MatchString(line) {
+		match := display_pattern_literal.FindStringSubmatch(line)
+
+		if osname == "linux" {
+			content := "echo '" + match[1] + "'\n"
+			write_to_script(flname, content)
+		} else {
+			// Win Code here
+		}
+
+	} else if display_pattern_no_new_line.MatchString(line) {
+		match := display_pattern_no_new_line.FindStringSubmatch(line)
+
+		if osname == "linux" {
+			content := "echo -n \"" + match[1] + "\"\n"
+			write_to_script(flname, content)
+		} else {
+			// Win Code here
+		}
+
 	} else {
-		er := "\n\n033[1;91m Error on line %d: \033[1;93m%s\033[1;92m\nThe Correct syntax is Display(\"< Your Text here>\")\033[1;0m\n"
+		er := "\n\n033[1;91m Syntax Error on line %d: \033[1;93m%s\033[1;0m\n"
 		fmt.Printf(er, index+1, line)
 		os.Remove(flname)
 		os.Exit(1)
