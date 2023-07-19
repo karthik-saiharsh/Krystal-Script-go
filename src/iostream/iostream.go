db6c5cdd1cf7702d199348882765cee2cc591d87
Karthik Saiharsh <137990475+KS-the-visionary@users.noreply.github.com>
2023-07-19 16:35:07 +0530

    Add files via upload

--- src/iostream/iostream.go
+++ src/iostream/iostream.go
@@ -33,6 +33,21 @@ func Display(line string, osname string) string {
 	}
 }
 
+func Ask(line string, osname string) string {
+	/*
+		This function manages the Ask() command.
+		We use regex to see if the line matches the expected pattern.
+		If yes we get the content inside the "" and the viarable name.
+	*/
+	ask_pattern := regexp.MustCompile(`^Ask\s*\(\s*"(.*?)"\s*,\s*([a-zA-Z_]+)\s*\)$`)
+
+	if ask_pattern.MatchString(line) {
+		return return_ask(ask_pattern.FindStringSubmatch(line)[1], ask_pattern.FindStringSubmatch(line)[2], osname)
+	} else {
+		return "err"
+	}
+}
+
 // These functions are what actually return the line that gets appended at the EOF that is generated
 
 func return_regular_display(str string, osname string) string {
@@ -64,5 +79,13 @@ func return_literal_nonewline_display(str string, osname string) string {
 		return fmt.Sprintf("echo -n '%s'", str)
 	} else {
 		return fmt.Sprintf("Write-Output -NoNewline '%s'", str)
+	}
+}
+
+func return_ask(ques string, variable string, osname string) string {
+	if osname == "linux" {
+		return fmt.Sprintf("read -p \"%s\" %s", ques, variable)
+	} else {
+		return fmt.Sprintf("$%s = Read-Host -Prompt \"%s\"", variable, ques)
 	}
 }
